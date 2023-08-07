44ed898ed51ea356c02f2edeb36682b28a63b0ee
Divya-BA <137373896+Divya-BA@users.noreply.github.com>
2023-08-07 20:21:27 +0530

    Add files via upload

---
+++ src/Price.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import './Price.css'
+import { BsX,BsCheck } from "react-icons/bs";
+
+
+function Price() {
+  return React.createElement('section',{className:'col-lg-12'},
+  React.createElement('div',{className:'container'},
+  React.createElement('div',{className:'row'},
+  React.createElement('div',{className:'col-lg-4'},
+  React.createElement('div',{className:'card mb-5 mb-lg-0'},
+  React.createElement('div',{className:'card-body'},
+  React.createElement('h5',{className:'card-title text-muted text-uppercase text-center'},'Free'),
+  React.createElement('h6',{className:'card-price text-center'},'$0',React.createElement('span',{className:'period'},'/month')),
+  React.createElement('hr',null),
+  React.createElement('ul',{className:'fa-ul'},
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Single User'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'5GB Storage'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Unlimited Public Project'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Community Access'),
+  React.createElement('li',{className:'text-muted'},React.createElement('span',{className:'fa-li'}),<BsX/>,'Unlimited Private Projects'),
+  React.createElement('li',{className:'text-muted'},React.createElement('span',{className:'fa-li'}),<BsX/>,'Dedicated phone Support'),
+  React.createElement('li',{className:'text-muted'},React.createElement('span',{className:'fa-li'}),<BsX/>,'Free Subdomain'),
+  React.createElement('li',{className:'text-muted'},React.createElement('span',{className:'fa-li'}),<BsX/>,'Monthly Status Report')),
+  React.createElement('div',{className:'d-grid'},React.createElement('a',{href:'#',className:'btn btn-primary text-uppercase'},'Button'))))),
+  //plus
+  React.createElement('div',{className:'col-lg-4'},
+  React.createElement('div',{className:'card mb-5 mb-lg-0'},
+  React.createElement('div',{className:'card-body'},
+  React.createElement('h5',{className:'card-title text-muted text-uppercase text-center'},'Free'),
+  React.createElement('h6',{className:'card-price text-center'},'$0',React.createElement('span',{className:'period'},'/month')),
+  React.createElement('hr',null),
+  React.createElement('ul',{className:'fa-ul'},
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Single User'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'5GB Storage'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Unlimited Public Project'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Community Access'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Unlimited Private Projects'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Dedicated phone Support'),
+  React.createElement('li',{className:'text-muted'},React.createElement('span',{className:'fa-li'}),<BsX/>,'Free Subdomain'),
+  React.createElement('li',{className:'text-muted'},React.createElement('span',{className:'fa-li'}),<BsX/>,'Monthly Status Report')),
+  React.createElement('div',{className:'d-grid'},React.createElement('a',{href:'#',className:'btn btn-primary text-uppercase'},'Button'))))),
+  //pro
+  React.createElement('div',{className:'col-lg-4'},
+  React.createElement('div',{className:'card mb-5 mb-lg-0'},
+  React.createElement('div',{className:'card-body'},
+  React.createElement('h5',{className:'card-title text-muted text-uppercase text-center'},'Free'),
+  React.createElement('h6',{className:'card-price text-center'},'$0',React.createElement('span',{className:'period'},'/month')),
+  React.createElement('hr',null),
+  React.createElement('ul',{className:'fa-ul'},
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Single User'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'5GB Storage'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Unlimited Public Project'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Community Access'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Unlimited Private Projects'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Dedicated phone Support'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Free Subdomain'),
+  React.createElement('li',React.createElement('span',{className:'fa-li'}),<BsCheck/>,'Monthly Status Report')),
+  React.createElement('div',{className:'d-grid'},React.createElement('a',{href:'#',className:'btn btn-primary text-uppercase'},'Button')))))
+  )))
+}
+
+export default Price
